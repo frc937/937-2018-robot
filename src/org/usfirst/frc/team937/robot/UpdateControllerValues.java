c1c30050353097ea17c5d88d79c1045ff15d8cdf
red2fred2 <admin@red2fred2.com>
2017-06-03 11:29:23 -0500

    Added a few  controller value updates

--- src/org/usfirst/frc/team937/robot/UpdateControllerValues.java
+++ src/org/usfirst/frc/team937/robot/UpdateControllerValues.java
@@ -2,4 +2,12 @@ package org.usfirst.frc.team937.robot;
 
 public class UpdateControllerValues {
 
+    public static boolean a;
+    public static boolean b;
+    public static boolean x;
+    public static boolean y;
+
+    public void UpdateControllerValues(Joystick controller) {
+       a = controller.getRawButton(RobotMap.controllerXNumber);
+    }
 }
