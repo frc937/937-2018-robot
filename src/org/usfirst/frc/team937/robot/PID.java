698727a909ee534ecf4230a09c57ad608b1d02a7
red2fred2 <admin@red2fred2.com>
2018-01-23 11:17:49 -0600

    constructed PID constructors

--- src/org/usfirst/frc/team937/robot/PID.java
+++ src/org/usfirst/frc/team937/robot/PID.java
@@ -8,5 +8,41 @@ import edu.wpi.first.wpilibj.*;
  * for when you need to get a thing to a place using a sensor
  */
 public class PID {
+
+    private double integeral = 0.0;
+    private double lastError = 0.0;
+
+    private double Pmultiplier;
+    private double Imultiplier;
+    private double Dmultiplier;
+
+    /**
+    * default constructor
+    */
+    public PID() {
+        Pmultiplier = 1.0;
+        Imultiplier = 1.0;
+        Dmultiplier = 1.0;
+    }
+
+    /**
+    * constructor with multipliers
+    * @param p
+    *   p multiplier
+    * @param i
+    *   i multiplier
+    * @param d
+    *   d multiplier
+    */
+    public PID(double p, double i, double d) {
+        Pmultiplier = p;
+        Imultiplier = i;
+        Dmultiplier = d;
+    }
+
+
+
+
+
     
 }
